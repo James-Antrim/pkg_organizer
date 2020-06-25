209e779695a24c450b38ab195d5259b705b9fe75
James Antrim <james.antrim@nm.thm.de>
2020-06-25 10:52:48 +0200

    Added the ability to apply/save and import to the subject edit view. Made the LSF ID a required field for subjects.

--- com_organizer/site/Views/HTML/SubjectEdit.php
+++ com_organizer/site/Views/HTML/SubjectEdit.php
@@ -31,25 +31,32 @@ class SubjectEdit extends EditView
 	 */
 	protected function addToolBar()
 	{
-		if ($this->item->id)
+		if ($new = empty($this->item->id))
 		{
-			$apply  = 'ORGANIZER_APPLY';
-			$cancel = 'ORGANIZER_CLOSE';
-			$save   = 'ORGANIZER_SAVE';
-			$title  = "ORGANIZER_SUBJECT_EDIT";
+			$apply       = Helpers\Languages::_('ORGANIZER_CREATE');
+			$applyImport = Helpers\Languages::_('ORGANIZER_CREATE_IMPORT');
+			$cancel      = Helpers\Languages::_('ORGANIZER_CLOSE');
+			$save        = Helpers\Languages::_('ORGANIZER_CREATE_CLOSE');
+			$saveImport  = Helpers\Languages::_('ORGANIZER_CREATE_IMPORT_CLOSE');
+			$title       = Helpers\Languages::_('ORGANIZER_SUBJECT_NEW');
+
 		}
 		else
 		{
-			$apply  = 'ORGANIZER_CREATE';
-			$cancel = 'ORGANIZER_CANCEL';
-			$save   = 'ORGANIZER_CREATE';
-			$title  = "ORGANIZER_SUBJECT_NEW";
+			$apply       = Helpers\Languages::_('ORGANIZER_APPLY');
+			$applyImport = Helpers\Languages::_('ORGANIZER_APPLY_UPDATE');
+			$cancel      = Helpers\Languages::_('ORGANIZER_CANCEL');
+			$save        = Helpers\Languages::_('ORGANIZER_SAVE');
+			$saveImport  = Helpers\Languages::_('ORGANIZER_SAVE_UPDATE');
+			$title       = Helpers\Languages::_('ORGANIZER_SUBJECT_EDIT');
 		}
 
 		Helpers\HTML::setTitle(Helpers\Languages::_($title), 'book');
 		$toolbar = Toolbar::getInstance();
-		$toolbar->appendButton('Standard', 'apply', Helpers\Languages::_($apply), 'subjects.apply', false);
-		$toolbar->appendButton('Standard', 'save', Helpers\Languages::_($save), 'subjects.save', false);
-		$toolbar->appendButton('Standard', 'cancel', Helpers\Languages::_($cancel), 'subjects.cancel', false);
+		$toolbar->appendButton('Standard', 'save-new', $apply, 'subjects.apply', false);
+		$toolbar->appendButton('Standard', 'file-add', $applyImport, 'subjects.applyImport', false);
+		$toolbar->appendButton('Standard', 'publish', $save, 'subjects.save', false);
+		$toolbar->appendButton('Standard', 'file-check', $saveImport, 'subjects.saveImport', false);
+		$toolbar->appendButton('Standard', 'cancel', $cancel, 'subjects.cancel', false);
 	}
 }
