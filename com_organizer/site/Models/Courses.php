11d33ab6f8da8564dde82ac5a54351410df997a0
James Antrim <james.antrim@nm.thm.de>
2020-09-05 18:37:36 +0200

    Removed references for potential future uses. Removed the output of the termID filter in front end. Added a status filter based on the course's enddate.

--- com_organizer/site/Models/Courses.php
+++ com_organizer/site/Models/Courses.php
@@ -23,7 +23,7 @@ class Courses extends ListModel
 
 	protected $defaultOrdering = 'name';
 
-	protected $filter_fields = ['campusID', 'categoryID', 'organizationID', 'groupID', 'personID', 'termID'];
+	protected $filter_fields = ['campusID', 'status', 'termID'];
 
 	/**
 	 * Filters out form inputs which should not be displayed due to menu settings.
@@ -35,12 +35,16 @@ class Courses extends ListModel
 	protected function filterFilterForm(&$form)
 	{
 		parent::filterFilterForm($form);
-		if ($this->clientContext === self::BACKEND)
+
+		if ($this->clientContext)
 		{
 			return;
 		}
 
+		$form->removeField('termID', 'filter');
+
 		$params = Helpers\Input::getParams();
+
 		if ($params->get('campusID'))
 		{
 			$form->removeField('campusID', 'filter');
@@ -48,21 +52,7 @@ class Courses extends ListModel
 
 		if ($params->get('onlyPrepCourses'))
 		{
-			$form->removeField('categoryID', 'filter');
-			$form->removeField('organizationID', 'filter');
-			$form->removeField('groupID', 'filter');
-			$form->removeField('personID', 'filter');
 			$form->removeField('search', 'filter');
-		}
-		elseif (empty($this->state->get('filter.organizationID')))
-		{
-			$form->removeField('categoryID', 'filter');
-			$form->removeField('personID', 'filter');
-			$form->removeField('groupID', 'filter');
-		}
-		elseif (empty($this->state->get('filter.categoryID')))
-		{
-			$form->removeField('groupID', 'filter');
 		}
 	}
 
@@ -75,41 +65,28 @@ class Courses extends ListModel
 	{
 		$tag   = Helpers\Languages::getTag();
 		$query = $this->_db->getQuery(true);
-		$query->select("c.*, c.name_$tag AS name")
+		$query->select("c.*, c.name_$tag AS name, MIN(u.startDate) AS startDate, MAX(u.endDate) AS endDate")
 			->from('#__organizer_courses AS c')
 			->innerJoin('#__organizer_units AS u ON u.courseID = c.id')
 			->innerJoin('#__organizer_instances AS i ON i.unitID = u.id')
 			->innerJoin('#__organizer_events AS e ON e.id = i.eventID')
-			->innerJoin('#__organizer_instance_persons AS ip ON ip.instanceID = i.id')
-			->innerJoin('#__organizer_instance_groups AS ig ON ig.assocID = ip.id')
-			->innerJoin('#__organizer_groups AS g ON g.id = ig.groupID')
-			->innerJoin('#__organizer_associations AS a ON a.categoryID = g.categoryID')
-			->where("u.delta != 'removed'")
-			->where("i.delta != 'removed'")
 			->group('c.id');
 
-		if ($search = $this->state->get('filter.search', '') and preg_match('/^[\d]+$/', $search))
-		{
-			$query->where("c.id = $search");
-		}
-		else
-		{
-			$this->setSearchFilter($query, ['c.name_de', 'c.name_en', 'e.name_de', 'e.name_en']);
-		}
+		$this->setSearchFilter($query, ['c.name_de', 'c.name_en', 'e.name_de', 'e.name_en']);
 
-		if ($this->clientContext === self::FRONTEND and Helpers\Input::getParams()->get('onlyPrepCourses'))
+		if (!$this->clientContext and Helpers\Input::getParams()->get('onlyPrepCourses'))
 		{
-			$termID = Helpers\Terms::getPreviousID($this->state->get('filter.termID'));
-			$query->where("c.termID = $termID")->where('e.preparatory = 1');
+			$query->where('e.preparatory = 1');
 		}
 		else
 		{
 			$this->setValueFilters($query, ['c.termID']);
 		}
 
-		if ($this->state->get('filter.organizationID'))
+		if (empty($this->state->get('filter.status')))
 		{
-			$this->setValueFilters($query, ['g.categoryID', 'a.organizationID', 'ig.groupID', 'ip.personID']);
+			$today = date('Y-m-d');
+			$query->where("endDate >= '$today'");
 		}
 
 		$this->addCampusFilter($query, 'c');
@@ -128,38 +105,15 @@ class Courses extends ListModel
 	protected function populateState($ordering = null, $direction = null)
 	{
 		parent::populateState($ordering, $direction);
-		$app = Helpers\OrganizerHelper::getApplication();
 
 		if ($this->clientContext === self::FRONTEND)
 		{
 			$params = Helpers\Input::getParams();
 
-			$campusID = $params->get('campusID', 0);
-			if (!$campusID)
+			if ($campusID = $params->get('campusID'))
 			{
-				$campusID = $app->getUserStateFromRequest($this->context . '.filter.campusID', 'filter.campusID');
+				$this->state->set('filter.campusID', $campusID);
 			}
-			$this->state->set('filter.campusID', $campusID);
-
-			$rTermID = $app->getUserStateFromRequest($this->context . '.filter.termID', 'filter.termID');
-			if (!$rTermID and $params->get('onlyPrepCourses'))
-			{
-				$this->state->set('filter.termID', Helpers\Terms::getNextID());
-			}
-		}
-
-		$organizationID = $app->getUserStateFromRequest($this->context . '.filter.organizationID',
-			'filter.organizationID');
-		$categoryID     = $app->getUserStateFromRequest($this->context . '.filter.categoryID', 'filter.categoryID');
-		if (empty($organizationID))
-		{
-			$this->state->set('filter.categoryID', '');
-			$this->state->set('filter.groupID', '');
-			$this->state->set('filter.personID', '');
-		}
-		elseif (empty($categoryID))
-		{
-			$this->state->set('filter.groupID', '');
 		}
 	}
 }
