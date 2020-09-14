c255068169675c327b4f34de6f48b96310d59ec7
James Antrim <james.antrim@nm.thm.de>
2020-09-14 14:49:18 +0200

    Moved existing PDF views for course participants to deployment.

--- com_organizer/site/Models/Organizer.php
+++ com_organizer/site/Models/Organizer.php
@@ -85,6 +85,84 @@ class Organizer extends BaseModel
 	}*/
 
 	/**
+	 * Migrates the participants of a given course from the old structure to the new structure.
+	 *
+	 * @return bool
+	 */
+	/*public function migrateParticipants()
+	{
+		$courseID = Helpers\Input::getSelectedID();
+
+		// Empty course
+		if (!$unitIDs = Helpers\Courses::getUnitIDs($courseID))
+		{
+			return true;
+		}
+
+		$unitIDs = implode(', ', $unitIDs);
+		$query   = $this->_db->getQuery(true);
+		$query->select('*')->from('#__thm_organizer_user_lessons')->where("lessonID IN ($unitIDs)");
+		$this->_db->setQuery($query);
+
+		// No baggage for the unit
+		if (!$participants = Helpers\OrganizerHelper::executeQuery('loadAssocList', []))
+		{
+			return true;
+		}
+
+		$cConditions   = 'c.schedule_date = b.date AND c.startTime = b.startTime AND c.endTime = b.endTime';
+		$cConditions   .= ' AND c.lessonID = i.unitID';
+		$ccmConditions = 'ccm.calendarID = c.id AND ccm.configurationID = lc.id';
+		$lsConditions  = 'ls.subjectID = i.eventID AND ls.lessonID = i.unitID';
+		$instanceQuery = $this->_db->getQuery(true);
+		$instanceQuery->select('i.id')
+			->from('#__organizer_instances AS i')
+			->innerJoin("#__thm_organizer_lesson_subjects AS ls ON $lsConditions")
+			->innerJoin('#__thm_organizer_lesson_configurations AS lc ON lc.lessonID = ls.id')
+			->innerJoin('#__organizer_blocks AS b ON b.id = i.blockID')
+			->innerJoin("#__thm_organizer_calendar AS c ON $cConditions")
+			->innerJoin("#__thm_organizer_calendar_configuration_map AS ccm ON $ccmConditions");
+
+		$deleteQuery = $this->_db->getQuery(true);
+		$deleteQuery->delete('#__organizer_user_lessons');
+
+		foreach ($participants as $participant)
+		{
+			$unitID = $participant['lessonID'];
+
+			$instanceQuery->clear('where')->where("i.unitID = $unitID");
+			$this->_db->setQuery($instanceQuery);
+
+			if ($instanceIDs = Helpers\OrganizerHelper::executeQuery('loadColumn', []))
+			{
+				$cpData            = ['courseID' => $courseID, 'participantID' => $participant['userID']];
+				$courseParticipant = new Tables\CourseParticipants();
+				if (!$courseParticipant->load($cpData))
+				{
+					$cpData['participantDate'] = $participant['user_date'];
+					$cpData['status']          = $participant['status'];
+					$cpData['statusDate']      = $participant['status_date'];
+					$cpData['attended']        = true;
+					$cpData['paid']            = true;
+					$courseParticipant->save($cpData);
+				}
+
+				foreach ($instanceIDs as $instanceID)
+				{
+					$ipData              = ['instanceID' => $instanceID, 'participantID' => $participant['userID']];
+					$instanceParticipant = new Tables\InstanceParticipants();
+					if (!$instanceParticipant->load($ipData))
+					{
+						$instanceParticipant->save($ipData);
+					}
+				}
+			}
+		}
+
+		return true;
+	}*/
+
+	/**
 	 * Migrates user lessons.
 	 *
 	 * @return bool true on success, otherwise false.
