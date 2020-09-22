40619bf3f80b9306086cf8960854837c90cf3e43
James Antrim <james.antrim@nm.thm.de>
2020-09-22 08:43:14 +0200

    Added the field type box a wrapper for textarea. Added minimum dimensions for textarea elements. Changed the body field from textarea to Box to ensure localization occurs.

---
+++ com_organizer/site/Fields/BoxField.php
@@ -0,0 +1,72 @@
+<?php
+/**
+ * @package     Organizer
+ * @extension   com_organizer
+ * @author      James Antrim, <james.antrim@nm.thm.de>
+ * @copyright   2020 TH Mittelhessen
+ * @license     GNU GPL v.3
+ * @link        www.thm.de
+ */
+
+namespace Organizer\Fields;
+
+use Joomla\CMS\Form\FormField;
+use Organizer\Helpers;
+
+/**
+ * Class creates text input.
+ */
+class BoxField extends FormField
+{
+	use Translated;
+
+	/**
+	 * The form field type.
+	 *
+	 * @var    string
+	 * @since  1.7.0
+	 */
+	protected $type = 'Box';
+
+	/**
+	 * Method to get the field input markup.
+	 *
+	 * @return  string  The field input markup.
+	 *
+	 * @since   1.7.0
+	 */
+	protected function getInput()
+	{
+		if ($this->hint and $hint = trim($this->hint))
+		{
+			$hint = preg_match('/^[A-Z_]+$/', $hint) ?
+				Helpers\Languages::_("ORGANIZER_$hint") : htmlspecialchars($hint, ENT_COMPAT, 'UTF-8');
+		}
+		else
+		{
+			$hint = '';
+		}
+
+		$attributes = [
+			(!$this->autocomplete or $this->autocomplete !== 'off') ?
+				'' : "autocomplete=\"$this->autocomplete\"",
+			$this->autofocus ? 'autofocus' : '',
+			$this->class ? "class=\"$this->class\"" : '',
+			$this->disabled ? 'disabled' : '',
+			$hint ? "placeholder=\"$hint\"" : '',
+			"id=\"$this->id\"",
+			$this->maxLength ? 'maxlength="' . (int) $this->maxLength . '"' : '',
+			"name=\"$this->name\"",
+			!empty($this->onChange) ? "onChange=\"$this->onChange\"" : '',
+			$this->pattern ? 'pattern="' . $this->pattern . '"' : '',
+			$this->readonly ? 'readonly' : '',
+			$this->required ? 'required aria-required="true"' : '',
+			$this->spellcheck ? '' : 'spellcheck="false"'
+		];
+
+		$open  = '<textarea ' . implode(' ', $attributes) . '>';
+		$value = htmlspecialchars($this->value, ENT_COMPAT, 'UTF-8');
+
+		return $open . $value . '</textarea>';
+	}
+}
