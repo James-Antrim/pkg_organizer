a1b9d4430159515f7e4773cc62668a28cbe92f0d
James Antrim <james.antrim@nm.thm.de>
2020-09-24 11:16:33 +0200

    Revert "First move for merge implementation."
    This reverts commit 6e0fe940

---
+++ com_organizer/site-staging/Models/Merge/MergeModels/PersonMerge.php
@@ -0,0 +1,353 @@
+<?php
+/**
+ * @package     Organizer
+ * @extension   com_organizer
+ * @author      James Antrim, <james.antrim@nm.thm.de>
+ * @copyright   2020 TH Mittelhessen
+ * @license     GNU GPL v.3
+ * @link        www.thm.de
+ */
+
+namespace Organizer\Models;
+
+use Organizer\Helpers;
+use Organizer\Tables;
+
+/**
+ * Class which manages stored person data.
+ */
+class PersonMerge extends MergeModel implements ScheduleResource
+{
+	protected $association = 'personID';
+
+	/**
+	 * Aggregates the attributes/resources associated with a person for a particular instance.
+	 *
+	 * @param   array  $assocs  the attributes/resources associated with the persons before the merge process.
+	 *
+	 * @return array the aggregated associations
+	 */
+	private function aggregateAssocs($assocs)
+	{
+		if (count($assocs) === 1)
+		{
+			return $assocs[0];
+		}
+
+		$groups  = [];
+		$roleIDs = [];
+		$rooms   = [];
+		foreach ($assocs as $assoc)
+		{
+			foreach ($assoc['groups'] as $groupID)
+			{
+				$groups[$groupID] = $groupID;
+			}
+
+			if ($roleIDs[$assoc['roleID']])
+			{
+				$roleIDs[$assoc['roleID']]++;
+			}
+			else
+			{
+				$roleIDs[$assoc['roleID']] = 1;
+			}
+
+			foreach ($assoc['rooms'] as $roomID)
+			{
+				$rooms[$roomID] = $roomID;
+			}
+		}
+
+		$roleID = array_keys($roleIDs, max($roleIDs))[0];
+
+		return ['groups' => array_values($groups), 'roleID' => $roleID, 'rooms' => array_values($rooms)];
+	}
+
+	/**
+	 * Authorizes the user.
+	 *
+	 * @return void
+	 */
+	protected function authorize()
+	{
+		if (!Helpers\Can::edit('persons', $this->selected))
+		{
+			Helpers\OrganizerHelper::error(403);
+		}
+	}
+
+	/**
+	 * Method to get a table object, load it if necessary.
+	 *
+	 * @param   string  $name     The table name. Optional.
+	 * @param   string  $prefix   The class prefix. Optional.
+	 * @param   array   $options  Configuration array for model. Optional.
+	 *
+	 * @return Tables\Persons A Table object
+	 *
+	 * @SuppressWarnings(PHPMD.UnusedFormalParameter)
+	 */
+	public function getTable($name = '', $prefix = '', $options = [])
+	{
+		return new Tables\Persons;
+	}
+
+	/**
+	 * Updates key references to the entry being merged.
+	 *
+	 * @return boolean  true on success, otherwise false
+	 */
+	protected function updateAssociations()
+	{
+		if (!$this->updateDRAssociation())
+		{
+			return false;
+		}
+
+		if (!$this->updateEventCoordinators())
+		{
+			return false;
+		}
+
+		if (!$this->updateInstancePersons())
+		{
+			return false;
+		}
+
+		return $this->updateSubjectPersons();
+	}
+
+	/**
+	 * Updates the event coordinators table to reflect the merge of the persons.
+	 *
+	 * @return bool true on success, otherwise false;
+	 */
+	private function updateEventCoordinators()
+	{
+		if (!$relevantEvents = $this->getAssociatedResourceIDs('eventID', 'event_coordinators'))
+		{
+			return true;
+		}
+
+		$mergeID = reset($this->selected);
+
+		foreach ($relevantEvents as $eventID)
+		{
+			$existing = new Tables\EventCoordinators;
+			$exists   = $existing->load(['eventID' => $eventID, 'personID' => $mergeID]);
+
+			foreach ($this->selected as $personID)
+			{
+				$ecTable        = new Tables\EventCoordinators;
+				$loadConditions = ['eventID' => $eventID, 'personID' => $personID];
+				if (!$ecTable->load($loadConditions))
+				{
+					continue;
+				}
+
+				if ($exists)
+				{
+					if ($existing->id !== $ecTable->id)
+					{
+						$ecTable->delete();
+					}
+
+					continue;
+				}
+
+				$existing = $ecTable;
+				$exists   = true;
+			}
+
+			$existing->personID = $mergeID;
+			if (!$existing->store())
+			{
+				return false;
+			}
+		}
+
+		return true;
+	}
+
+	/**
+	 * Updates the instance persons table to reflect the merge of the persons.
+	 *
+	 * @return bool true on success, otherwise false;
+	 */
+	private function updateInstancePersons()
+	{
+		if (!$relevantInstances = $this->getAssociatedResourceIDs('instanceID', 'instance_persons'))
+		{
+			return true;
+		}
+
+		$mergeID = reset($this->selected);
+
+		foreach ($relevantInstances as $instanceID)
+		{
+			$delta    = '';
+			$modified = '';
+			$roleID   = '';
+
+			$existing = new Tables\InstancePersons;
+			$exists   = $existing->load(['instanceID' => $instanceID, 'personID' => $mergeID]);
+
+			foreach ($this->selected as $personID)
+			{
+				$ipTable        = new Tables\InstancePersons;
+				$loadConditions = ['instanceID' => $instanceID, 'personID' => $personID];
+				if (!$ipTable->load($loadConditions))
+				{
+					continue;
+				}
+
+				if ($ipTable->modified > $modified)
+				{
+					$delta    = $ipTable->delta;
+					$modified = $ipTable->modified;
+					$roleID   = $ipTable->roleID;
+				}
+
+				if ($exists)
+				{
+					if ($existing->id !== $ipTable->id)
+					{
+						$ipTable->delete();
+					}
+
+					continue;
+				}
+
+				$exists   = true;
+				$existing = $ipTable;
+			}
+
+			$existing->delta    = $delta;
+			$existing->modified = $modified;
+			$existing->personID = $mergeID;
+			$existing->roleID   = $roleID;
+			if (!$existing->store())
+			{
+				return false;
+			}
+		}
+
+		return true;
+	}
+
+	/**
+	 * Updates the subject persons table to reflect the merge of the persons.
+	 *
+	 * @return bool true on success, otherwise false;
+	 */
+	private function updateSubjectPersons()
+	{
+		$mergeIDs = implode(', ', $this->selected);
+		$query    = $this->_db->getQuery(true);
+		$query->select("DISTINCT subjectID, role")
+			->from("#__organizer_subject_persons")
+			->where("personID IN ($mergeIDs)");
+		$this->_db->setQuery($query);
+		if (!$relevantAssocs = Helpers\OrganizerHelper::executeQuery('loadAssocList', []))
+		{
+			return true;
+		}
+
+		$mergeID          = reset($this->selected);
+		$responsibilities = [];
+
+		foreach ($relevantAssocs as $assoc)
+		{
+			$subjectID = $assoc['subjectID'];
+			if (empty($responsibilities[$subjectID]))
+			{
+				$responsibilities[$subjectID] = [];
+			}
+
+			$responsibilities[$subjectID][$assoc['role']] = $assoc['role'];
+		}
+
+		foreach ($responsibilities as $subjectID => $roles)
+		{
+			foreach ($roles as $role)
+			{
+				$existing    = new Tables\SubjectPersons;
+				$entryExists = $existing->load(['personID' => $mergeID, 'role' => $role, 'subjectID' => $subjectID]);
+
+				foreach ($this->selected as $personID)
+				{
+					$spTable        = new Tables\SubjectPersons;
+					$loadConditions = ['personID' => $personID, 'role' => $role, 'subjectID' => $subjectID];
+					if (!$spTable->load($loadConditions))
+					{
+						continue;
+					}
+
+					if ($entryExists)
+					{
+						if ($existing->id !== $spTable->id)
+						{
+							$spTable->delete();
+						}
+
+						continue;
+					}
+
+					$entryExists = true;
+					$existing    = $spTable;
+				}
+
+				$existing->personID = $mergeID;
+				if (!$existing->store())
+				{
+					return false;
+				}
+			}
+		}
+
+		return true;
+	}
+
+	/**
+	 * Processes the data for an individual schedule
+	 *
+	 * @param   Tables\Schedules  $schedule  the schedule being processed
+	 *
+	 * @return bool true if the schedule was changed, otherwise false
+	 */
+	public function updateSchedule($schedule)
+	{
+		$instances = json_decode($schedule->schedule, true);
+		$mergeID   = reset($this->selected);
+		$relevant  = false;
+
+		foreach ($instances as $instanceID => $persons)
+		{
+			if (!$relevantPersons = array_intersect(array_keys($persons), $this->selected))
+			{
+				continue;
+			}
+
+			$relevant = true;
+
+			$assocs = [];
+			foreach ($relevantPersons as $personID)
+			{
+				$assocs[] = $instances[$instanceID][$personID];
+				unset($instances[$instanceID][$personID]);
+			}
+
+			$instances[$instanceID][$mergeID] = $this->aggregateAssocs($assocs);
+		}
+
+		if ($relevant)
+		{
+			$schedule->schedule = json_encode($instances);
+
+			return true;
+		}
+
+		return false;
+	}
+}
