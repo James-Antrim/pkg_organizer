1d34eeee4cb02196abe690acfe69061792786e2c
James Antrim <james.antrim@nm.thm.de>
2020-09-28 10:27:11 +0200

    Added a class to model a deprecated table for the transition period.

---
+++ com_organizer/site/Tables/LessonConfigurations.php
@@ -0,0 +1,52 @@
+<?php
+/**
+ * @package     Organizer
+ * @extension   com_organizer
+ * @author      James Antrim, <james.antrim@nm.thm.de>
+ * @copyright   2020 TH Mittelhessen
+ * @license     GNU GPL v.3
+ * @link        www.thm.de
+ */
+
+namespace Organizer\Tables;
+
+use JDatabaseDriver;
+
+/**
+ * Models the organizer_instances table.
+ */
+class LessonConfigurations extends BaseTable
+{
+	/**
+	 * A configuration object modeled by a JSON string, containing the room/teacher configuration.
+	 * TEXT
+	 *
+	 * @var string
+	 */
+	public $configuration;
+
+	/**
+	 * The id of the unit entry referenced.
+	 * INT(11) UNSIGNED NOT NULL
+	 *
+	 * @var int
+	 */
+	public $lessonID;
+
+	/**
+	 * The time at which this entry was last modified.
+	 *
+	 * @var int
+	 */
+	public $modified;
+
+	/**
+	 * Declares the associated table
+	 *
+	 * @param   JDatabaseDriver  $dbo  A database connector object
+	 */
+	public function __construct($dbo = null)
+	{
+		parent::__construct('#__thm_organizer_lesson_configurations', 'id', $dbo);
+	}
+}
