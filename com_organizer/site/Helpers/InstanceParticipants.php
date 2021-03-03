eefda7b860515085a47c3af2a7ec7e67aab2fb28
James Antrim <james.antrim@nm.thm.de>
2021-03-03 14:05:47 +0100

    Added helper to get the booking id associated with a participation entry.

---
+++ com_organizer/site/Helpers/InstanceParticipants.php
@@ -0,0 +1,38 @@
+<?php
+/**
+ * @package     Organizer
+ * @extension   com_organizer
+ * @author      James Antrim, <james.antrim@nm.thm.de>
+ * @copyright   2020 TH Mittelhessen
+ * @license     GNU GPL v.3
+ * @link        www.thm.de
+ */
+
+namespace Organizer\Helpers;
+
+use Organizer\Tables;
+
+/**
+ * Class provides generalized functions regarding dates and times.
+ */
+class InstanceParticipants
+{
+	/**
+	 * Returns the color value for a given colorID.
+	 *
+	 * @param   int  $participationID  the id of the color
+	 *
+	 * @return int the id of the booking associated with the participation
+	 */
+	public static function getBookingID(int $participationID): int
+	{
+		$participation = new Tables\InstanceParticipants();
+
+		if (!$participation->load($participationID))
+		{
+			return 0;
+		}
+
+		return Instances::getBookingID($participation->instanceID);
+	}
+}
