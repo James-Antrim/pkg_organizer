726d18f02e67907fcb3ace527242a2df466e8fda
James Antrim <james.antrim@nm.thm.de>
2021-04-07 15:47:13 +0200

    Initial commit for a block selection field. - Form values for date and grid id must have already been set. - The grid must have periods. - Blocks are dynamically supplemented on a needs basis.

---
+++ com_organizer/site-staging/Fields/BlocksField.php
@@ -0,0 +1,76 @@
+<?php
+/**
+ * @package     Organizer
+ * @extension   com_organizer
+ * @author      James Antrim, <james.antrim@nm.thm.de>
+ * @copyright   2020 TH Mittelhessen
+ * @license     GNU GPL v.3
+ * @link        www.thm.de
+ */
+
+namespace Organizer\Fields;
+
+use Organizer\Helpers;
+use Organizer\Helpers\HTML;
+use Organizer\Tables\Blocks;
+
+/**
+ * Class creates a select box for predefined colors.
+ */
+class BlocksField extends OptionsField
+{
+	/**
+	 * Type
+	 *
+	 * @var    String
+	 */
+	protected $type = 'Blocks';
+
+	/**
+	 * Method to get the field options.
+	 *
+	 * @return  array  The field option objects.
+	 */
+	protected function getOptions(): array
+	{
+		$form    = $this->form;
+		$options = parent::getOptions();
+
+		// Basic conditions not met
+		if (!$date = $form->getValue('date') or !$gridID = $form->getValue('gridID'))
+		{
+			return $options;
+		}
+
+		if (!$grid = Helpers\Grids::getGrid($gridID))
+		{
+			return $options;
+		}
+
+		$grid = json_decode($grid, true);
+
+		if (!is_array($grid) or !array_key_exists('periods', $grid) or !$periods = $grid['periods'])
+		{
+			return $options;
+		}
+
+		foreach ($periods as $period)
+		{
+			$endTime   = Helpers\Dates::formatTime($period['endTime']);
+			$startTime = Helpers\Dates::formatTime($period['startTime']);
+			$keys      = ['date' => $date, 'endTime' => $endTime, 'startTime' => $startTime];
+
+			$block = new Blocks();
+
+			if (!$block->load($keys))
+			{
+				$keys['dow'] = date('w', strtotime($date));
+				$block->save($keys);
+			}
+
+			$options[] = HTML::_('select.option', $block->id, "$startTime - $endTime");
+		}
+
+		return $options;
+	}
+}
