f6a7ebc4f6a0c1f23907a28a0a1d16bac6164ed4
James Antrim <james.antrim@nm.thm.de>
2021-04-20 11:50:35 +0200

    Added functions supporting instance management.

--- com_organizer/site/Controllers/Instances.php
+++ com_organizer/site/Controllers/Instances.php
@@ -11,8 +11,11 @@
 namespace Organizer\Controllers;
 
 use Exception;
+use Joomla\CMS\Factory;
+use Joomla\CMS\Router\Route;
 use Organizer\Controller;
 use Organizer\Helpers;
+use Organizer\Models\Instance;
 
 /**
  * Class receives user actions and performs access checks and redirection.
@@ -22,6 +25,39 @@ class Instances extends Controller
 	protected $listView = 'instances';
 
 	protected $resource = 'instance';
+
+	/**
+	 * Function to add a new instance not related to an event. Existing appointments can be edited directly using the
+	 * edit function.
+	 *
+	 * @return void
+	 * @throws Exception
+	 */
+	public function appointment()
+	{
+		Helpers\Input::set('appointment', true);
+		$this->edit();
+	}
+
+	/**
+	 * Ends the instance create/edit process and empties the session container.
+	 *
+	 * @return void
+	 */
+	public function cancel()
+	{
+		$session  = Factory::getSession();
+		$instance = $session->get('organizer.instance', []);
+
+		if (!empty($instance['referrer']))
+		{
+			Helpers\Input::set('referrer', $instance['referrer']);
+		}
+
+		$session->set('organizer.instance', '');
+
+		parent::cancel();
+	}
 
 	/**
 	 * Prints badges for the selected participants.
@@ -47,5 +83,61 @@ class Instances extends Controller
 		Helpers\Input::set('format', 'pdf');
 		Helpers\Input::set('layout', 'GridA4');
 		parent::display();
+	}
+
+	/**
+	 * Removed all properties stored in the session
+	 *
+	 * @return void
+	 */
+	public function reset()
+	{
+		$session  = Factory::getSession();
+		$instance = $session->get('organizer.instance', []);
+
+		if (!empty($instance['referrer']))
+		{
+			$instance = ['referrer' => $instance['referrer']];
+		}
+
+		$session->set('organizer.instance', $instance);
+
+		parent::cancel();
+	}
+
+	/**
+	 * Save form data to the database.
+	 *
+	 * @return void
+	 */
+	public function save()
+	{
+		$model    = new Instance();
+		$session  = Factory::getSession();
+		$instance = $session->get('organizer.instance', []);
+		$referrer = empty($instance['referrer']) ? '' : $instance['referrer'];
+
+		if ($model->save())
+		{
+			Helpers\OrganizerHelper::message('ORGANIZER_SAVE_SUCCESS');
+			$session->set('organizer.instance', '');
+			$this->setRedirect(Route::_($referrer, false));
+		}
+
+		Helpers\OrganizerHelper::message('ORGANIZER_SAVE_FAIL', 'error');
+
+		$url = Helpers\Routing::getRedirectBase() . "&view=instance_edit";
+
+		if ($id = Helpers\Input::getID())
+		{
+			$url .= "&id=$id";
+		}
+
+		if (Helpers\Input::getBool('appointment'))
+		{
+			$url .= '&appointment=1';
+		}
+
+		$this->setRedirect(Route::_($url, false));
 	}
 }
