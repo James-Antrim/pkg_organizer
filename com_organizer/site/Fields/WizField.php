04a7bb88ac46919ebd469e453a5046147ea6c002
James Antrim <james.antrim@nm.thm.de>
2021-06-25 10:07:41 +0200

    Fixes Joomla editor field labeling error in front end through inheritance.

---
+++ com_organizer/site/Fields/WizField.php
@@ -0,0 +1,28 @@
+<?php
+/**
+ * @package     Organizer
+ * @extension   com_organizer
+ * @author      James Antrim, <james.antrim@nm.thm.de>
+ * @copyright   2020 TH Mittelhessen
+ * @license     GNU GPL v.3
+ * @link        www.thm.de
+ */
+
+namespace Organizer\Fields;
+
+use Joomla\CMS\Form\Field\EditorField;
+
+/**
+ * Class creates text input.
+ */
+class WizField extends EditorField
+{
+	use Translated;
+
+	/**
+	 * The form field type.
+	 *
+	 * @var    string
+	 */
+	public $type = 'Wiz';
+}
