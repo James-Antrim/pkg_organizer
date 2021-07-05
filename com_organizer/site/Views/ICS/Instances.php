8d19f8b3cb685213a194406f08fbca4b98862c7c
James Antrim <james.antrim@nm.thm.de>
2021-07-05 13:22:04 +0200

    Removed the second attempt at modelling ics resources bottom up. Added a flat strategy, because most components will not be used and the components which will be used calendar, event and timezone reuse the same data extensively.

--- com_organizer/site/Views/ICS/Instances.php
+++ com_organizer/site/Views/ICS/Instances.php
@@ -11,9 +11,11 @@
 
 namespace Organizer\Views\ICS;
 
+use Exception;
 use Organizer\Helpers;
 use Organizer\Models;
 use Organizer\Views\ICS\Components\VCalendar;
+use SimpleXMLElement;
 
 /**
  * Base class for a Joomla View
@@ -29,6 +31,38 @@ class Instances
 	 */
 	private $instances;
 
+	private $language;
+
+	/**
+	 * This property specifies the date and time (UTC) that the instance of the iCalendar object was created.
+	 * @var string
+	 * @url https://datatracker.ietf.org/doc/html/rfc5545#section-3.8.7.2
+	 */
+	private $stamp;
+
+	/**
+	 * This property specifies the text value that uniquely identifies the "VTIMEZONE" calendar component in the scope
+	 * of an iCalendar object.
+	 *
+	 * @var string
+	 * @url https://datatracker.ietf.org/doc/html/rfc5545#section-3.8.3.1
+	 */
+	private $tzID;
+
+	/**
+	 * The component version.
+	 *
+	 * @var string
+	 */
+	private $version;
+
+	/**
+	 * This property defines the persistent, globally unique identifier for the calendar component.
+	 * @var string
+	 * @url https://datatracker.ietf.org/doc/html/rfc5545#section-3.8.4.7
+	 */
+	private $uniqueID;
+
 	/**
 	 * Performs initial construction of the TCPDF Object.
 	 */
@@ -41,23 +75,103 @@ class Instances
 			Helpers\OrganizerHelper::error(404);
 		}
 
-		/*$conditions = $model->conditions;
+		$this->language = strtoupper(Helpers\Languages::getTag());
+		$this->tzID     = date_default_timezone_get();
 
-		if (empty($conditions['my']))
-		{
-			if ($conditions['groupIDs'])
-			{
-				unset($conditions['organizationIDs']);
-			}
+		$this->setStamp();
+		$this->setVersion();
+		$this->setUID();
+	}
 
+	/**
+	 * Provide a grouping of component properties that describe an event.
+	 *
+	 * @param   array   &$ics       the output container
+	 * @param   object   $instance  the instance being iterated
+	 *
+	 * @return void modifies $ics
+	 * @url https://datatracker.ietf.org/doc/html/rfc5545#section-3.6.1
+	 */
+	private function addEvent(array &$ics, object $instance)
+	{
+		$date      = $instance->date;
+		$endTime   = $instance->endTime;
+		$startTime = $instance->startTime;
 
-		}*/
+		$ics[] = "BEGIN:VEVENT";
+		$ics[] = $this->stamp;
+		$ics[] = $this->uniqueID;
+		$ics[] = 'DTSTART' . $this->getDateTime("$date $startTime");
+		$ics[] = 'DTEND' . $this->getDateTime("$date $endTime");
+		$ics[] = $this->getLastModified($instance);
 
-		//groups rooms, persons categories campuses
-		//echo "<pre>" . print_r($conditions, true) . "</pre><br>";
-		/*
-		 * my => title = ~Stundenplan <Full Name> ; prodid
+		/**
+		 * The following are OPTIONAL but MUST NOT occur more than once:
+		 *
+		 * created
+		 *
+		 *
+		 * description (TEXT):
+		 * This property provides a more complete description of the calendar component than that provided by the "SUMMARY" property.
+		 * https://datatracker.ietf.org/doc/html/rfc5545#section-3.8.1.5
+		 *
+		 * geo (CS-FLOAT):
+		 * This property specifies information related to the global position for the activity specified by a calendar component.
+		 * The value MUST be two SEMICOLON-separated FLOAT values.
+		 * https://datatracker.ietf.org/doc/html/rfc5545#section-3.8.1.6
+		 *
+		 * location
+		 *
+		 *
+		 * organizer
+		 *
+		 *
+		 * priority
+		 *
+		 *
+		 * seq
+		 *
+		 *
+		 * status
+		 *
+		 *
+		 * summary
+		 *
+		 *
+		 * url
+		 *
+		 *
+		 * recurid
 		 */
+
+		/**
+		 * The following are OPTIONAL, and MAY occur more than once:
+		 *
+		 * attach
+		 * attendee
+		 * categories
+		 * comment
+		 * contact
+		 * exdate
+		 * rstatus
+		 * related
+		 * resources
+		 * rdate
+		 * x-prop
+		 * iana-prop
+		 */
+
+		//DESCRIPTION:https://moodle.thm.de/course/view.php?id=4221
+		//LOCATION:ONLINE
+		//ORGANIZER:MAILTO:james.antrim@nm.thm.de
+		//PRIORITY:5
+		//SEQUENCE:0
+		//SUMMARY:Leit- und Sicherungstechnik - VRL gehalten von Riesbeck\, T..
+
+		// @url https://datatracker.ietf.org/doc/html/rfc5545#section-3.8.2.7
+		$ics[] = "TRANSP:OPAQUE";
+
+		$ics[] = "END:VEVENT";
 	}
 
 	/**
@@ -83,12 +197,124 @@ class Instances
 	 */
 	public function display()
 	{
-		$calendar = new VCalendar($this->instances);
-		$ics      = [];
-		$calendar->fill($ics);
+		$ics   = [];
+		$ics[] = "BEGIN:VCALENDAR";
+		$ics[] = "PRODID:-//TH Mittelhessen//NONSGML Organizer $this->version//$this->language";
+		$ics[] = 'VERSION:' . $this->version;
+		$ics[] = "METHOD:PUBLISH";
 
+		//XPROPS
+		//TIMEZONE
+		echo "<pre>" . print_r(reset($this->instances), true) . "</pre><br>";
+
+		foreach ($this->instances as $instance)
+		{
+			$this->addEvent($ics, $instance);
+		}
+
+		$ics[] = "END:VCALENDAR";
+		echo "<pre>" . print_r($ics, true) . "</pre><br>";
 		die;
 		//$this->Output($this->filename, $destination);
 		//ob_flush();
+	}
+
+	/**
+	 * Formats a given date time string (Y-m-d H:i) to a timezone qualified DATE-TIME.
+	 *
+	 * @param   string  $dateTime
+	 *
+	 * @return string
+	 * @url https://datatracker.ietf.org/doc/html/rfc5545#section-3.3.5
+	 */
+	private function getDateTime(string $dateTime = '')
+	{
+		$stamp = preg_match('/^\d{4}-\d{2}-\d{2} \d{2}:\d{2}$/', $dateTime) === false ? time() : strtotime($dateTime);
+		$value = date('Ymd', $stamp) . 'T' . date('His', $stamp);
+
+		return ";$this->tzID:$value";
+	}
+
+	/**
+	 * This property specifies the date and time that the information associated with the calendar component was last
+	 * revised in the calendar store.
+	 *
+	 * @param   object  $instance  the instance
+	 *
+	 * @return string the last modified property
+	 * @url https://datatracker.ietf.org/doc/html/rfc5545#section-3.8.7.3
+	 */
+	private function getLastModified(object $instance): string
+	{
+		$lastModified = $instance->instanceStatusDate;
+		$lastModified = $lastModified > $instance->unitStatusDate ? $lastModified : $instance->unitStatusDate;
+
+		if (!empty($instance->resources))
+		{
+			foreach ($instance->resources as $person)
+			{
+				$lastModified = $lastModified > $person['statusDate'] ? $lastModified : $person['statusDate'];
+
+				if (!empty($person['groups']))
+				{
+					foreach ($person['groups'] as $group)
+					{
+						$lastModified = $lastModified > $group['statusDate'] ? $lastModified : $group['statusDate'];
+					}
+				}
+
+				if (!empty($person['rooms']))
+				{
+					foreach ($person['rooms'] as $room)
+					{
+						$lastModified = $lastModified > $room['statusDate'] ? $lastModified : $room['statusDate'];
+					}
+				}
+			}
+		}
+
+		$stamp = strtotime($lastModified);
+		$date  = gmdate('Ymd', $stamp);
+		$time  = gmdate('His', $stamp);
+
+		return "LAST-MODIFIED:{$date}T{$time}Z";
+	}
+
+	/**
+	 * Sets the stamp (UTC generation time) to be used for all components.
+	 *
+	 * @return void
+	 */
+	private function setStamp()
+	{
+		$date = gmdate('Ymd');
+		$time = gmdate('His');
+
+		$this->stamp = "DTSTAMP:{$date}T{$time}Z";
+	}
+
+	/**
+	 * Sets the class $version property from the component manifest.
+	 *
+	 * @return void
+	 */
+	private function setVersion()
+	{
+		$manifest = JPATH_ADMINISTRATOR . '/components/com_organizer/com_organizer.xml';
+
+		try
+		{
+			$manifest      = new SimpleXMLElement(file_get_contents($manifest));
+			$this->version = (string) $manifest->version;
+		}
+		catch (Exception $exception)
+		{
+			$this->version = "X.X.X";
+		}
+	}
+
+	private function setUID()
+	{
+		$this->uniqueID = "UID:THEUNIQUEID";
 	}
 }
