4db05034c7fb51c7dc2e6186a324e67d9705e043
James Antrim <james.antrim@nm.thm.de>
2021-07-19 12:56:10 +0200

    Finished first iteration of the internally developed ics export. - Output is now functional and valid. - Revised unique ids to be unique to the individual components they are a part of and not the single calendar component. - Added escaping for protected characters in text values. - Added single resolution for URLs and Moodle/Panopto IDs in the instance comments. - Added comments to the output. - Added chunking to ensure compliance with the hard limit of 75 characters. - Corrected error where the version property was referring to the component version and not the ics format version. - Corrected error in datetime value formatting. - Reformatted calendar titles to be more relevant in the user's list of subscribed calendars.

--- com_organizer/site/Views/ICS/Instances.php
+++ com_organizer/site/Views/ICS/Instances.php
@@ -29,7 +29,33 @@ use SimpleXMLElement;
  */
 class Instances
 {
-	private const INITIAL_RELEASE = 0, HOUR = 3600, MEDIUM = 5, MINUTE = 60, SPEAKER = 4, TEACHER = 1, TUTOR = 3;
+	/**
+	 * Component internal fixed roles.
+	 */
+	private const SPEAKER = 4, TEACHER = 1, TUTOR = 3;
+
+	/**
+	 * ICS format values.
+	 */
+	private const INITIAL_RELEASE = 0, LINE_LIMIT = 75, MEDIUM = 5;
+
+	/**
+	 * Time units in seconds; used for offset calculations.
+	 */
+	private const HOUR = 3600, MINUTE = 60;
+
+	/**
+	 * Hexidecimal byte values used for resolving character encoding.
+	 */
+	private const ONE_BYTE_LOWER = 0x20, ONE_BYTE_UPPER = 0x7F, TWO_BYTE_MARK = 0xC0, THREE_BYTE_MARK = 0xE0,
+		FOUR_BYTE_MARK = 0xF0, FIVE_BYTE_MARK = 0xF8, SIX_BYTE_MARK = 0xFC, SEVEN_BYTE_MARK = 0xFE;
+
+	/**
+	 * The name of the generated file.
+	 *
+	 * @var string
+	 */
+	private $fileName;
 
 	/**
 	 * The instances data.
@@ -81,11 +107,12 @@ class Instances
 	private $version;
 
 	/**
-	 * This property defines the persistent, globally unique identifier for the calendar component.
+	 * The template for uIDs used in individual components.
+	 *
 	 * @var string
 	 * @url https://datatracker.ietf.org/doc/html/rfc5545#section-3.8.4.7
 	 */
-	private $uniqueID;
+	private $uIDTemplate;
 
 	/**
 	 * @var User
@@ -98,12 +125,17 @@ class Instances
 	public function __construct()
 	{
 		$model = new Models\Instances();
+		$uri   = Uri::getInstance();
 
-		$this->language  = strtoupper(Helpers\Languages::getTag());
-		$this->instances = $model->getItems();
-		$this->state     = $model->getState();
-		$this->tzID      = date_default_timezone_get();
-		$this->user      = Helpers\Users::getUser();
+		$left  = date('Ymd') . 'T' . date('His') . date('T');
+		$right = $uri->getHost() . $uri->getPath();
+
+		$this->language    = strtoupper(Helpers\Languages::getTag());
+		$this->instances   = $model->getItems();
+		$this->state       = $model->getState();
+		$this->tzID        = date_default_timezone_get();
+		$this->user        = Helpers\Users::getUser();
+		$this->uIDTemplate = "UID:$left-%s@$right";
 
 		$this->setStamp();
 		$this->setTitles();
@@ -191,9 +223,7 @@ class Instances
 
 		$ics[] = "BEGIN:VEVENT";
 		$ics[] = $this->stamp;
-
-		// TODO this should be unique to the component (event) the ical guys implemented it slightly wrong here.
-		$ics[] = $this->uniqueID;
+		$ics[] = sprintf($this->uIDTemplate, $instance->instanceID);
 		$ics[] = 'DTSTART' . $this->getDateTime("$date $startTime");
 		$ics[] = 'DTEND' . $this->getDateTime("$date $endTime");
 
@@ -217,6 +247,7 @@ class Instances
 			}
 
 			$description = sprintf(Helpers\Languages::_('ORGANIZER_ICS_DESCRIPTION'), $description);
+			$description = $this->escape($description);
 
 			$ics[] = "DESCRIPTION:$description";
 		}
@@ -259,13 +290,15 @@ class Instances
 				$location = implode(', ', $locations) . " & $last";
 			}
 
+			$location = $this->escape($location);
+
 			$ics[] = "LOCATION:$location";
 		}
 
 		// @url https://datatracker.ietf.org/doc/html/rfc5545#section-3.8.4.3
 		if ($this->user->email)
 		{
-			$ics[] = "ORGANIZER:mailto:$this->user->email";
+			$ics[] = 'ORGANIZER:mailto:' . $this->user->email;
 		}
 
 		// @url https://datatracker.ietf.org/doc/html/rfc5545#section-3.8.1.9
@@ -287,23 +320,22 @@ class Instances
 		// @url https://datatracker.ietf.org/doc/html/rfc5545#section-3.8.1.12
 		$summary = $instance->name;
 		$summary .= $method ? " - $instance->method" : '';
+		$summary = $this->escape($summary);
 		$ics[]   = "SUMMARY:$summary";
 
 		/**
-		 * The following are OPTIONAL but MUST NOT occur more than once:
-		 * url
+		 * url?
+		 * @url https://datatracker.ietf.org/doc/html/rfc5545#section-3.8.4.6
 		 *
-		 *
-		 * recurid
+		 * comment*
+		 * @url https://datatracker.ietf.org/doc/html/rfc5545#section-3.8.1.4
 		 */
+		$this->setCommentnURL($ics, $instance->comment);
 
 		/**
-		 * The following are OPTIONAL, and MAY occur more than once:
-		 * comment
-		 * contact
+		 * TODO: Add contact information for the site administration?
+		 * @url https://datatracker.ietf.org/doc/html/rfc5545#section-3.8.4.2
 		 */
-
-		//DESCRIPTION:https://moodle.thm.de/course/view.php?id=4221
 
 		// @url https://datatracker.ietf.org/doc/html/rfc5545#section-3.8.2.7
 		$ics[] = "TRANSP:OPAQUE";
@@ -312,19 +344,144 @@ class Instances
 	}
 
 	/**
-	 * Chunks the lines into segments no greater than 75 Bytes for standards conformity.
+	 * Enforces the 75 byte line limitation by 'folding'.
 	 *
-	 * @param   string  $output  the output string to be chunked
+	 * @param   string  $buffer  the input string used as a byte buffer during processing
 	 *
 	 * @return string
+	 * @url https://datatracker.ietf.org/doc/html/rfc5545#section-3.1
 	 */
-	private function chunk(string $output): string
+	private function chunk(string $buffer): string
 	{
-		// chunk it
-		// add "\r\n "
-		// implode it
+		if (strlen($buffer) <= self::LINE_LIMIT)
+		{
+			return $buffer;
+		}
 
-		return 'CHUNKED OUTPUT';
+		$currentLine = '';
+		$lines       = [];
+		$pos         = 0;
+		$return      = '';
+
+		while (true)
+		{
+			// All characters have been processed.
+			if (!isset($buffer[$pos]))
+			{
+				$lines[] = $currentLine;
+				break;
+			}
+
+			// The previous iteration ended the line.
+			if (strlen($currentLine) === self::LINE_LIMIT)
+			{
+				$lines[]     = $currentLine;
+				$currentLine = '';
+			}
+
+			// Remove processed characters from the buffer;
+			$buffer = substr($buffer, $pos);
+			$pos    = 0;
+
+			// The unprocessed portion of the buffer is under limit with consideration of the space from the fold
+			if (!$currentLine and strlen($buffer) <= 74)
+			{
+				$lines[] = " $buffer";
+				break;
+			}
+
+			$current = $buffer[0];
+			$next1   = $buffer[1] ?? '';
+			$next2   = $buffer[2] ?? '';
+			$next3   = $buffer[3] ?? '';
+			$next4   = $buffer[4] ?? '';
+			$next5   = $buffer[5] ?? '';
+
+			// New line sequence would get cut.
+			if (strlen($currentLine) === 74 and $current === '\\' and $next1 === 'n')
+			{
+				// End current line
+				$lines[] = $currentLine;
+
+				// New line would only have the new line sequence making it superfluous.
+				if (!$next2)
+				{
+					break;
+				}
+
+				// Start new line with the new line sequence and tell the buffer where to start
+				$currentLine = ' \n';
+				$pos         = 2;
+
+				continue;
+			}
+
+			$byte        = ord($current);
+			$currentLine .= $current;
+
+			/**
+			 * After one byte encoding has been ruled out a bit-wise and is made with the next higher byte prefix to
+			 * suppress insignificant bytes for the comparison with the actual byte prefix to ensure character encoding.
+			 */
+
+			// Characters U-00000000 - U-0000007F (same as ASCII)
+			if ($byte >= self::ONE_BYTE_LOWER and $byte <= self::ONE_BYTE_UPPER)
+			{
+				$pos++;
+				continue;
+			}
+
+			// Characters U-00000080 - U-000007FF, mark 110XXXXX
+			if ($byte & self::THREE_BYTE_MARK === self::TWO_BYTE_MARK)
+			{
+				if (isset($next1))
+				{
+					$currentLine .= $next1;
+					$pos++;
+				}
+			}
+			// Characters U-00000800 - U-0000FFFF, mark 1110XXXX
+			elseif ($byte & self::FOUR_BYTE_MARK === self::THREE_BYTE_MARK)
+			{
+				if (isset($next2))
+				{
+					$return .= $next1 . $next2;
+					$pos    += 2;
+				}
+			}
+			// Characters U-00010000 - U-001FFFFF, mark 11110XXX
+			elseif ($byte & self::FIVE_BYTE_MARK === self::FOUR_BYTE_MARK)
+			{
+				if (isset($next3))
+				{
+					$return .= $next1 . $next2 . $next3;
+					$pos    += 3;
+				}
+			}
+			// Characters U-00200000 - U-03FFFFFF, mark 111110XX
+			elseif ($byte & self::SIX_BYTE_MARK === self::FIVE_BYTE_MARK)
+			{
+				if (isset($next4))
+				{
+					$return .= $next1 . $next2 . $next3 . $next4;
+					$pos    += 4;
+				}
+			}
+			// Characters U-04000000 - U-7FFFFFFF, mark 1111110
+			elseif ($byte & self::SEVEN_BYTE_MARK === self::SIX_BYTE_MARK)
+			{
+				if (isset($next5))
+				{
+					$return .= $next1 . $next2 . $next3 . $next4 . $next5;
+					$pos    += 5;
+				}
+			}
+
+			// Move forward in the buffer
+			$pos++;
+		}
+
+		return implode("\r\n", $lines);
 	}
 
 	/**
@@ -337,22 +494,20 @@ class Instances
 	{
 		$ics   = [];
 		$ics[] = 'BEGIN:VCALENDAR';
+		$ics[] = "VERSION:2.0";
 		$ics[] = "PRODID:-//TH Mittelhessen//NONSGML Organizer $this->version//$this->language";
-		$ics[] = "VERSION:$this->version";
 		$ics[] = 'METHOD:PUBLISH';
 		$ics[] = "X-WR-CALNAME:$this->title";
 		$ics[] = "X-WR-TIMEZONE:$this->tzID";
 		$ics[] = 'BEGIN:VTIMEZONE';
 		$ics[] = "TZID:$this->tzID";
 		$ics[] = 'BEGIN:STANDARD';
-		$ics[] = 'DTSTART:' . $this->getDateTime();
+		$ics[] = 'DTSTART' . $this->getDateTime();
 		$ics[] = 'TZNAME:Standard Time';
 		$ics[] = 'TZOFFSETFROM:' . $this->getOffset('2021-01-01');
 		$ics[] = 'TZOFFSETTO:' . $this->getOffset('2021-07-01');
 		$ics[] = "END:STANDARD";
 		$ics[] = "END:VTIMEZONE";
-
-		//echo "<pre>" . print_r(reset($this->instances), true) . "</pre><br>";
 
 		foreach ($this->instances as $instance)
 		{
@@ -360,10 +515,56 @@ class Instances
 		}
 
 		$ics[] = "END:VCALENDAR";
-		echo "<pre>" . print_r($ics, true) . "</pre><br>";
-		die;
-		//$this->Output($this->filename, $destination);
-		//ob_flush();
+
+		foreach ($ics as $index => $line)
+		{
+			$ics[$index] = $this->chunk($line);
+		}
+
+		$fsize  = 0;
+		$output = implode("\r\n", $ics) . "\r\n";
+
+		if ($temp = tempnam(sys_get_temp_dir(), 'ics'))
+		{
+			if (file_put_contents($temp, $output))
+			{
+				$fsize = filesize($temp);
+			}
+
+			unlink($temp);
+		}
+
+		if ($fsize)
+		{
+			header("Content-Length: $fsize");
+		}
+
+		header('Content-Type: text/calendar; charset=utf-8');
+		header('Content-Disposition: attachment; filename="' . $this->fileName . '"');
+		header('Cache-Control: max-age=10');
+		echo $output;
+		ob_flush();
+	}
+
+	/**
+	 * Escape special character use in text values.
+	 *
+	 * @param   string  $text
+	 *
+	 * @return string
+	 */
+	private function escape(string $text): string
+	{
+		$text = preg_replace('/\\\\([^nNr,;])/', '\\\\\\\\$1', $text);
+
+		$map = ['"' => "'", ',' => '\,', ';' => '\;', "\r\n" => '\n', "\r" => '\n', "\n" => '\n', '\N' => '\n'];
+
+		foreach ($map as $from => $to)
+		{
+			$text = str_replace($from, $to, $text);
+		}
+
+		return $text;
 	}
 
 	/**
@@ -380,7 +581,7 @@ class Instances
 		$stamp    = strtotime($dateTime);
 		$value    = date('Ymd', $stamp) . 'T' . date('His', $stamp);
 
-		return ";$this->tzID:$value";
+		return ";TZID=$this->tzID:$value";
 	}
 
 	/**
@@ -428,6 +629,119 @@ class Instances
 	}
 
 	/**
+	 * Sets the vevent comment and url, which would otherwise be in the comment. Removes all regexed URLs from the
+	 * comment. As per RFC only one URL is added, even if multiple were present.
+	 *
+	 * @param   array   &$ics      the output container
+	 * @param   string   $comment  the commentary for the vevent
+	 *
+	 * @return void
+	 * @url https://datatracker.ietf.org/doc/html/rfc5545#section-3.8.1.4
+	 * @url https://datatracker.ietf.org/doc/html/rfc5545#section-3.8.4.6
+	 */
+	private function setCommentnURL(array &$ics, string $comment = '')
+	{
+		if (!$comment)
+		{
+			return;
+		}
+
+		$url = '';
+
+		$moodleCourse = 'URL:https://moodle.thm.de/course/view.php?id=PID';
+
+		// Moodle Course 1
+		if (preg_match('/(((https?):\/\/)moodle.thm.de\/course\/view.php\?id=(\d+))/', $comment, $matches))
+		{
+			$comment = trim(str_replace($matches[0], '', $comment));
+			$url     = str_replace('PID', $matches[4], $moodleCourse);
+		}
+
+		// Moodle Course 2
+		if (preg_match('/moodle=(\d+)/', $comment, $matches))
+		{
+			$comment = trim(str_replace($matches[0], '', $comment));
+
+			if (!$url)
+			{
+				$url = str_replace('PID', $matches[1], $moodleCourse);
+			}
+		}
+
+		$moodleCategory = 'URL:https://moodle.thm.de/course/index.php?categoryid=PID';
+
+		// Moodle Category
+		if (preg_match('/(((https?):\/\/)moodle\.thm\.de\/course\/index\.php\\?categoryid=(\\d+))/', $comment, $matches))
+		{
+			$comment = trim(str_replace($matches[0], '', $comment));
+
+			if (!$url)
+			{
+				$url = str_replace('PID', $matches[4], $moodleCategory);
+			}
+		}
+
+		// NetAcademy
+		if (preg_match('/(((https?):\/\/)\d+.netacad.com\/courses\/\d+)/', $comment, $matches))
+		{
+			$thisURL = $matches[0];
+			$comment = trim(str_replace($thisURL, '', $comment));
+
+			if (!$url)
+			{
+				$url = "URL:$thisURL";
+			}
+		}
+
+		$panopto = 'URL:https://panopto.thm.de/Panopto/Pages/Viewer.aspx?id=PID';
+
+		// Panopto 1
+		if (preg_match('/(((https?):\/\/)panopto.thm.de\/Panopto\/Pages\/Viewer.aspx\?id=[\d\w\-]+)/', $comment, $matches))
+		{
+			$comment = trim(str_replace($matches[0], '', $comment));
+
+			if (!$url)
+			{
+				$url = str_replace('PID', $matches[4], $panopto);
+			}
+		}
+
+		// Panopto 2
+		if (preg_match('/panopto=([\d\w\-]+)/', $comment, $matches))
+		{
+			$comment = trim(str_replace($matches[0], '', $comment));
+
+			if (!$url)
+			{
+				$url = str_replace('PID', $matches[1], $panopto);
+			}
+		}
+
+		// Pilos
+		if (preg_match('/(((https?):\/\/)(\d+|roxy).pilos-thm.de\/(b\/)?[\d\w]{3}-[\d\w]{3}-[\d\w]{3})/', $comment, $matches))
+		{
+			$thisURL = $matches[0];
+			$comment = trim(str_replace($thisURL, '', $comment));
+
+			if (!$url)
+			{
+				$url = "URL:$thisURL";
+			}
+		}
+
+		if ($url)
+		{
+			$ics[] = $url;
+		}
+
+		if ($comment)
+		{
+			$comment = $this->escape($comment);
+			$ics[]   = "COMMENT:$comment";
+		}
+	}
+
+	/**
 	 * Sets the stamp (UTC generation time) to be used for all components.
 	 *
 	 * @return void
@@ -448,70 +762,66 @@ class Instances
 	private function setTitles()
 	{
 		$state = $this->state;
+		$user  = $this->user;
+		$title = Helpers\Languages::_('ORGANIZER_INSTANCES');
 
-		echo "<pre>" . print_r($state, true) . "</pre><br>";
-		$uri  = Uri::getInstance();
-		$user = $this->user;
-
-		$left      = date('Ymd') . 'T' . date('His') . date('T');
-		$middle    = '';
-		$right     = $uri->getHost() . $uri->getPath();
-		$title     = Helpers\Languages::_('ORGANIZER_INSTANCES');
-		$tTemplate = $title . ": %s";
-		$uTemplate = "UID:$left-%s@$right";
+		$suffix        = ".ics";
+		$preTemplate   = "$title: %s";
+		$postTemplate  = "%s $title";
+		$postTemplate2 = "%s - Organizer $title";
 
 		if ($state->get('filter.my') and $user->username)
 		{
-			$this->title    = sprintf($tTemplate, $user->name);
-			$this->uniqueID = sprintf($uTemplate, $user->username);
+			$this->title    = Helpers\Languages::_('ORGANIZER_INSTANCES_ICS');
+			$this->fileName = OutputFilter::stringURLSafe($this->title) . $suffix;
 
 			return;
 		}
 
 		if ($thisID = (int) $state->get('filter.eventID'))
 		{
-			$this->title    = sprintf($tTemplate, Helpers\Events::getName($thisID));
-			$this->uniqueID = sprintf($uTemplate, Helpers\Events::getCode($thisID));
+			$this->title    = sprintf($postTemplate, Helpers\Events::getName($thisID));
+			$this->fileName = OutputFilter::stringURLSafe($this->title) . $suffix;
 
 			return;
 		}
 
 		if ($thisID = (int) $state->get('filter.personID'))
 		{
-			$this->title    = sprintf($tTemplate, Helpers\Persons::getDefaultName($thisID));
-			$this->uniqueID = sprintf($uTemplate, Helpers\Persons::getCode($thisID));
+			$this->title    = sprintf($postTemplate2, Helpers\Persons::getDefaultName($thisID, true));
+			$this->fileName = OutputFilter::stringURLSafe($this->title) . $suffix;
 
 			return;
 		}
 
 		if ($thisID = (int) $state->get('filter.groupID'))
 		{
-			$this->title    = sprintf($tTemplate, Helpers\Groups::getFullName($thisID));
-			$this->uniqueID = sprintf($uTemplate, Helpers\Groups::getCode($thisID));
+			$this->title    = sprintf($postTemplate, Helpers\Groups::getFullName($thisID));
+			$this->fileName = OutputFilter::stringURLSafe($this->title) . $suffix;
 
 			return;
 		}
 
 		if ($thisID = (int) $state->get('filter.roomID'))
 		{
-			$this->title    = sprintf($tTemplate, Helpers\Rooms::getName($thisID));
-			$this->uniqueID = sprintf($uTemplate, Helpers\Persons::getCode($thisID));
+			$this->title    = sprintf($preTemplate, Helpers\Rooms::getName($thisID));
+			$this->fileName = OutputFilter::stringURLSafe($this->title) . $suffix;
 
 			return;
 		}
 
 		if ($thisID = (int) $state->get('filter.categoryID'))
 		{
-			$this->title    = sprintf($tTemplate, Helpers\Categories::getName($thisID));
-			$this->uniqueID = sprintf($uTemplate, Helpers\Groups::getCode($thisID));
+			$this->title    = sprintf($postTemplate, Helpers\Categories::getName($thisID));
+			$this->fileName = OutputFilter::stringURLSafe($this->title) . $suffix;
 
 			return;
 		}
 
 		if ($thisID = (int) $state->get('filter.organizationID'))
 		{
-			$this->title    = sprintf($tTemplate, Helpers\Organizations::getName($thisID));
-			$this->uniqueID = sprintf($uTemplate, Helpers\Organizations::getAbbreviation($thisID));
+			$this->title    = sprintf($postTemplate, Helpers\Organizations::getName($thisID));
+			$this->fileName = OutputFilter::stringURLSafe($this->title) . $suffix;
 
 			return;
 		}
@@ -519,14 +829,14 @@ class Instances
 		if ($thisID = (int) $state->get('filter.campusID'))
 		{
 			$campus         = Helpers\Campuses::getName($thisID);
-			$this->title    = sprintf($tTemplate, $campus);
-			$this->uniqueID = sprintf($uTemplate, OutputFilter::stringURLSafe($campus));
+			$this->title    = sprintf($postTemplate, $campus);
+			$this->fileName = OutputFilter::stringURLSafe($this->title) . $suffix;
 
 			return;
 		}
 
 		$this->title    = $title;
-		$this->uniqueID = "UID:$left-$middle@$right";
+		$this->fileName = OutputFilter::stringURLSafe($this->title) . $suffix;
 	}
 
 	/**
