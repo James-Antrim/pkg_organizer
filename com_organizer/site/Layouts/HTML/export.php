d86fc0a386c8c9d49862bc09658bc613923cad2e
James Antrim <james.antrim@nm.thm.de>
2021-07-22 16:27:18 +0200

    Initial commit for the migrated export view. - Form manifest, layout, model, view. - Added styles to form css. - Added localization. - Deleted un-migrated staging files.

---
+++ com_organizer/site/Layouts/HTML/export.php
@@ -0,0 +1,66 @@
+<?php
+/**
+ * @package     Organizer
+ * @extension   com_organizer
+ * @author      James Antrim, <james.antrim@nm.thm.de>
+ * @copyright   2020 TH Mittelhessen
+ * @license     GNU GPL v.3
+ * @link        www.thm.de
+ */
+
+use Joomla\CMS\Uri\Uri;
+use Organizer\Adapters\Toolbar;
+use Organizer\Helpers;
+
+$query  = Uri::getInstance()->getQuery();
+$script = "document.getElementById('download-url').select();";
+$script .= "document.getElementById('download-url').setSelectionRange(0,99999);";
+$script .= "document.execCommand('copy');";
+
+echo Helpers\OrganizerHelper::getApplication()->JComponentTitle;
+?>
+    <form action="<?php echo Uri::current(); ?>" id="adminForm" method="post" name="adminForm"
+          class="form-horizontal form-validate" enctype="multipart/form-data">
+        <div class="export-container">
+			<?php
+			foreach ($this->form->getFieldSets() as $set)
+			{
+				$label  = $set->label ?: false;
+				$fields = $this->form->getFieldset($set->name);
+
+				if ($label)
+				{
+					$constant = 'ORGANIZER_' . strtoupper($label);
+					$label    = Helpers\Languages::_($constant);
+					echo "<fieldset class=\"organizer-$set->name\">";
+					echo "<legend>$label</legend>";
+				}
+
+				foreach ($fields as $field)
+				{
+					echo $field->renderField();
+				}
+
+				if ($label)
+				{
+					echo '</fieldset>';
+				}
+			}
+			?>
+        </div>
+        <input type="hidden" name="Itemid" value="<?php echo Helpers\Input::getInt('Itemid'); ?>"/>
+        <input type="hidden" name="option" value="com_organizer"/>
+        <input type="hidden" name="task" value=""/>
+        <input type="hidden" name="view" value="<?php echo $this->get('name'); ?>"/>
+		<?php echo Helpers\HTML::_('form.token'); ?>
+    </form>
+<?php echo Toolbar::getInstance()->render(); ?>
+<?php if ($this->url): ?>
+    <div class="tbox-green">
+        <h6><?php echo Helpers\Languages::_('ORGANIZER_DOWNLOAD_URL_DESC'); ?></h6>
+        <input type="text" id="download-url" class="download-url" value="<?php echo $this->url; ?>"/>
+        <button class="btn copy-button" onclick="<?php echo $script; ?>">
+            <span class="icon-copy"></span><?php echo Helpers\Languages::_('ORGANIZER_COPY'); ?>
+        </button>
+    </div>
+<?php endif; ?>
---
+++ com_organizer/site/Models/Export.php
@@ -0,0 +1,150 @@
+<?php
+/**
+ * @package     Organizer
+ * @extension   com_organizer
+ * @author      James Antrim, <james.antrim@nm.thm.de>
+ * @copyright   2020 TH Mittelhessen
+ * @license     GNU GPL v.3
+ * @link        www.thm.de
+ */
+
+namespace Organizer\Models;
+
+use Joomla\CMS\Form\Form;
+use Organizer\Helpers;
+use Organizer\Helpers\Input;
+
+/**
+ * Class retrieves information for a filtered set of instances.
+ */
+class Export extends FormModel
+{
+
+	/**
+	 * @inheritDoc
+	 */
+	public function __construct($config = [])
+	{
+		// Resolve potential inconsistencies cause by user choices before the form is initialized.
+		if ($task = Input::getTask() and $task === 'export.reset')
+		{
+			// The language should not be reset with the rest.
+			$form = ['languageTag' => Helpers\Languages::getTag()];
+		}
+		else
+		{
+			$fields = ['categoryID' => 0, 'groupID' => 0, 'organizationID' => 0, 'personID' => 0, 'roomID' => 0];
+			$form   = Input::getArray();
+
+			if (!empty($form['my']))
+			{
+				foreach (array_keys($fields) as $field)
+				{
+					unset($form[$field]);
+				}
+			}
+			else
+			{
+				/*$campusID = empty($form['campusID']) ? 0 : $form['campusID'];
+				$roomID   = empty($form['roomID']) ? 0 : $form['roomID'];
+
+				if ($campusID and $roomID)
+				{
+					if (!Helpers\Rooms::onCampus($roomID, $campusID))
+					{
+						unset($form['roomID']);
+					}
+				}*/
+
+				$categoryID     = empty($form['categoryID']) ? 0 : $form['categoryID'];
+				$organizationID = empty($form['organizationID']) ? 0 : $form['organizationID'];
+				$groupID        = empty($form['groupID']) ? 0 : $form['groupID'];
+				$personID       = empty($form['personID']) ? 0 : $form['personID'];
+				if ($organizationID)
+				{
+					if ($categoryID and !in_array($organizationID, Helpers\Categories::getOrganizationIDs($categoryID)))
+					{
+						$categoryID = 0;
+						unset($form['categoryID']);
+					}
+
+					if ($groupID and !in_array($organizationID, Helpers\Groups::getOrganizationIDs($groupID)))
+					{
+						$groupID = 0;
+						unset($form['groupID']);
+					}
+
+					if ($personID and !in_array($organizationID, Helpers\Persons::getOrganizationIDs($personID)))
+					{
+						unset($form['groupID']);
+					}
+				}
+
+				if ($categoryID and $groupID and $categoryID !== Helpers\Groups::getCategory($groupID)->id)
+				{
+					unset($form['groupID']);
+				}
+			}
+		}
+
+		// Post: where the data was actually transmitted
+		Input::set('jform', $form, 'post');
+
+		// Data: where Joomla preemptively aggregates request information
+		Input::set('jform', $form);
+
+		parent::__construct($config);
+	}
+
+	/**
+	 * Provides a strict access check which can be overwritten by extending classes.
+	 *
+	 * @return void performs error management via redirects as appropriate
+	 */
+	protected function authorize()
+	{
+		// Form has public access
+	}
+
+	/**
+	 * @inheritDoc
+	 */
+	protected function filterForm(Form $form)
+	{
+		if (!Helpers\Users::getID())
+		{
+			$form->removeField('personID');
+			$form->removeField('my');
+		}
+
+		if (Input::getBool('my'))
+		{
+			//$form->removeField('campusID');
+			$form->removeField('organizationID');
+			$form->removeField('categoryID');
+			$form->removeField('groupID');
+			$form->removeField('personID');
+			$form->removeField('roomID');
+		}
+		elseif (!Input::getInt('organizationID') and !Input::getInt('categoryID'))
+		{
+			$form->removeField('groupID');
+		}
+	}
+
+	/**
+	 * @inheritDoc
+	 */
+	public function getForm($data = [], $loadData = true)
+	{
+		return parent::getForm($data, $loadData);
+	}
+
+	/**
+	 * @inheritdoc
+	 */
+	protected function loadFormData(): array
+	{
+		return Input::getArray();
+	}
+}
