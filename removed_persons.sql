a9524d486167da161f8178a8ceeea6b4cf2e0354
James Antrim <james.antrim@nm.thm.de>
2022-08-03 17:39:20 +0200

    Added a wrapper for the driver's get null date function. Ensures that all saved schedule associations have a modified date.

---
+++ removed_persons.sql
@@ -0,0 +1,639 @@
+
+Ahlers, Hendrik: AhleH
+
+#region B
+Bartusek: Bart
+Becker: BeckF
+Bergmann: BergT
+Bergmann, Thorsten: BergT
+Blumberg, Igor: BlumI
+Böhm; Hans Jürgen: BöhmHJ
+Böttcher, Marc: BöttM
+Böttcher: BöttO
+Brück, Felix: BrücFe
+Brück-Lahiji, Christina: BrücC
+Bucys, Juliane: BucyJ
+Busweiler, Jörg-Ulrich: BuswJU
+#endregion
+
+#region C
+Carle, Wilfried: CarlW
+Celik, Yasemin: CeliY
+Chainani-Barta, Pritima: ChaiBP
+Cimen: CimeT
+Cimen, Tunay: CimeT
+Clausdorff, Leuder: ClauL
+Cypra, Thorsten: CyprT
+Czapiewski, Jürgen: CzapJ
+#endregion
+
+#region D
+Dammann, Vera: DammV
+Darmawi: DarmS
+Darmawi, Sabrina: DarmS
+David, Ioan: DaviI
+Deinert, Gesa Dörte Maria: DeinGDM
+Diehl, Rudolf: DiehR
+Ditter, Michael: DittM
+Dominik, Andreas: DomiA #manual removal of one
+Dominik, Andreas Peter: DomiAP #manual removal of one
+Dönges, Thorsten: DöngT
+Dörbecker: Dörb #manual removal
+Dörbecker, Susanne: DörbS
+Dos Santos, Maique: DosSM
+Dreßler, Diana: DreßD
+#endregion
+
+#region E
+Ecker, Erhardt: EckeE
+Eifler, Tanja: EiflT
+Eimer, Axel: EimeA
+Eisfeller, Guido: EisfG
+Elett, Ralf: EletR
+Elseberg, Christiane: ElseC
+Endres, Christian: EndrC
+Engel, Gerson: EngeG
+Engelhardt, Wibke: EngeW
+Eufinger, Chiara: EufiC
+#endregion
+
+#region F
+Fedorova, Ksenia: FedoK
+Feldrappe, Doris: FeldD
+Felsing, Sebastian Tobias: FelsST
+Feuerer: Feue
+Fischer, Julia Sarah: FiscJS
+Foerster, Jens: FoerJ
+Fonseca, Helena Fernandes: daFonsHF
+Foninha, Antonio: FontA
+Förster, Jens: FörsJ
+Frankford, Peter: FranP
+Fricke, Christina: FricC
+Fritzsch, Cornelia: FritC
+Fuchs, Roland: FuchR
+Funk, Jens Rüdiger: FunkJR
+#endregion
+
+# region G
+Gänselein, Martin: GänsM
+Garbrecht, Firedrich WIlhelm: GarbFW
+Gebler, Helmut: GeblH
+Gerlach, Markus: GerlM
+Gersch: Gers
+Geyer, Björn: GeyeB
+Giessen: GiesR
+Giessen, Rene: GiesR
+Goedecke, Falk: GoedF
+Görge: GörgB
+Görge, Benjamin: GörgB
+Gosen, Christina Elisabeth Anna: GoseCEA
+Götz, Peter: GötzP
+Götzelmann, Timo: GötzT
+Gregorkiewicz, Patrick: GregP
+Gromut, Karina: GromK
+Gröninger, Burkhard: GrönB
+Gruber, Marcus: GrubM
+Gutberlet, Rainer: GutbR
+#endregion
+
+#region H
+Haas, Alexander: HaasA #manual removal
+Haas, Alexander: HaasAl
+Haas, Annabel: HaasAn
+Hähn, Carsten: HähnC
+Hain, Franziska: HainF
+Halstenberg, Elke: HalsE
+Hammel, Sascha: HammelS
+Happel, Harald: HappH
+# Hardt, Detlef: HardD
+Harrer, Sebastian: HarrS
+Hartung: Hart
+Hasenkamp, Alexander: HaseA
+Heider-Knabe, Edda: HeidKE
+Heigele, Christian: HeigC
+Heilmann, Martin: HeilM
+Heils, Sigrid: HeilS
+Heiser, Marion: HeisM
+Henrich, Dirk: HenrD
+Henry, Lawrence: HenrL
+Henry, Laurence: HenrL
+Herchenhein, Nicola: HercN
+Herling, Otfried: HerlO
+Herrenbauer, Michael: HerrM
+Herrenbauer: HerrM
+Herzog, Judith Melanie: HerzJM
+Herzog, Sergej: HerzS
+Heynemann, Johanna: HeynJ
+Hilpert, André: HilpA
+Hobohm, Uwe: HoboU
+Hofmann, Karen: HofmK
+Hohenbild, Ute: HoheU
+Holaschke, Holger: HolaH
+Honal: Hona
+Hornig, Thorsten Alexander: HornTA
+Huang, Hefei: HuanH
+Hübner, Malte: HübnM
+Huschka, Karsten: HuscK
+Huschka: HuscK
+#endregion
+
+#region I
+Intra, Frank: IntrF
+Israel, Kerstin: IsraK
+#endregion
+
+#region J
+Jacobi, Tobias: JacoT
+Jäger, Carl-Philipp: JägeCP
+# Jäger, Eberhard: JägE
+Jäger, Rudolf: JägeR
+Jahnen, Peter: JahnP
+Jäkel, Andrea: JäkeA
+Janzen: Janz #manual removal
+Janzen, Andrea: JanzA
+John, Andreas: JohnA
+Jopp, Fred: JoppF
+Jullmann: Jull
+Jung, Hubert: JungHu0
+Jung, Ingo: JungI
+# Jung, Martin: JungM
+Jungkind, Anna:JungA
+#endregion
+
+#region K
+Käflein, Ruthhard: KäflR
+Kaiser, Andreas: KaisA
+Kaiser, Markus: KaisM
+Kaletsch, Heike: KaleH
+Kambeitz, Heinz-Ludwig: KambHL
+Kämpfer, Helmut: KämpH
+Kämpfer: Kämp #manual removal
+Karau, Andreas: KaraA
+Kasper: Kasp #manual removal
+Kasper, Erik: KaspE
+Kasteleiner, Björn: KastB
+Kaufmann, Erika: KaufE
+Kausen, Ernst: KausE
+Keiner, Heike: KeinH
+Keller, Lena: KellL
+Kempf, Natascha: KempN
+Kempf, Tobias: KempT
+Kersher, Klaus: KersK
+# Keßler, Sandra: KeßlS
+Kessler: KessG #manual removal
+Kessler, Gabriele: KessG #manual removal
+Kessler, Gabriele: KessGa
+Kirschbaum, Ulrich: KirsU
+Kirsten, Daniel: KirsD
+Klausmann, Tobias Johannes: KlauTJ
+Klein, Torsten: KleiT
+Kleinhans: Klei
+Kleinöder, Rudolf: KleiR
+Klos: Klos
+Klos, Artur: KlosA
+Knorz, Katherina: KnorK
+Knorz, Marc: KnorM
+Koch, Madeleine Christina: KochMC
+Koch, Peter: KochP
+Köhler, Ulrich: KöhlU
+Körber, Oliver: KörbO
+Kovács, Zoltán: KováZ
+Kraft, Elisabeth: KrafE
+Kraft, Kilian Jacob: KrafKJ
+Krähling, Dorothea: KrähD
+Krämer: Kräm
+Kramer, Dieter: KramD
+Kramer, Viktor: KramV
+Kraus, Alexander: KrauA
+Krebast, Dennis: KrebD
+Kriesmer, KrieT #manual removal
+Kriesmer, Thomas: KrieT #manual removal
+Kriesmer, Thomas: KrieTh
+Kromer: Krom
+Kröning, Michael: KrönM
+Krum, Alexander: KrumA
+Kuhl, Florian: KühlF #manual removal for one
+Kuhl, Florian: KuhlF #manual removal for one
+Kuhrt: Kuhr
+Kujaschewski, Patrizia: KujaP
+Kuntscher, Bernhard: KuntB
+Kuntze, Nicolai: KuntN
+Kupka, Thomas: KupkT
+#endregion
+
+#region L
+Lämmel, Andreas: LämmA
+Landau, Karl-Otto: LandKO
+Landgraf: Land
+Landrock-Bill, Elke: LandBE
+Lauwerth, Werner: LauwW
+Lechner: Lech
+Leicht: Leic
+Leidner, Jörg: LeidJ
+Lemmer, Norbert: LemmN
+Lemp, Lena: LempL
+Lenn: Lenn
+Lepper, Björn: LeppB
+Lich, Ottmar: LichO
+Lidwin, Michael: LidwM
+Liebehenschel: Lieb #manual removal
+Liebehenschel, Jens: LiebJ
+Lin-Hi, Nick: LinHN
+Lind, Tobias: LindT
+Linke, Burkhard: LinkB
+Lischka, Martin: LiscM
+Little, Simon: LittS
+# Lödel, Thmoas: LödT
+Löer, Katharina: Löer #manual removal
+Löer, Katharina: LöerK
+Löffler, Peter: LöffP
+Löhr, Benjamin: LöhrB
+# Lorenz: Lor
+Lorenz, Jörg: LoreJ
+Lorenz, Peter: LoreP
+Loth, Matthias: LothM
+Loth, Michael: LothMi
+Luig, Rainer: LuigR
+Luthardt, Stefan: LuthS
+Lüttebrandt, Michael: LüttM
+#endregion
+
+#region M
+Machmüller: MachA
+Machmüller, Andreas: MachA
+Mai, Lukas: MaiL
+Mather, Andreas: MathA
+May, Peter: MayP
+Mehler, Jens: MehlJ
+Mensch: Mens
+Meyer, Renate: MeyeR
+Michajlow, Ilja: MichI
+Möllenbeck, Sascha: MöllS
+Möller: MöllT
+Möller, Torben: MöllT
+Moosecker, Wolfgang: MoosW
+Morherr: Morh #manual removal
+Morherr, Frank: MorhF #manual removal
+Morherr, Frank Martin: MorhFM
+Müller: MüllO
+Müller, Fabian: MüllF
+Müller, Jan Marco: MüllJM
+Müller, Mailänder: MüllM
+Müller, Sina Catherina: MüllSC
+Müller, Wolfgang: MüllW
+Müller-Eibich, Stefanie: MüllES
+Mündel: MündN
+Mündel, Norbert: MündN
+Mursina, Ljudmila: Murs
+#endregion
+
+# region N
+Netz, Sigrid: NetzS
+Niazi, Abdullah-Bastian: NiazAB
+Niemann, Volker: NiemV
+Nietert, Manfred: NietM
+Nissen, Ulrich: NissU
+#endregion
+
+#region O
+# Ochs-Held: OchsKP
+# Ochs-Held, Peter: OchsHP
+#endregion
+
+#region P
+# Paleta, Miranda: PaleM
+Pallauf: Pall
+Pallauf, Carol: PallC
+Peppel, Jörg-Michael: PeppJM
+Peppler, Achim: PeppA
+Peschk-Aschenbrenner, Michaela Andrea: PescAMA
+Peter: Pete
+Pfaff, Matthias: PfafM
+Pfeifer, Yvonne: PfeiY
+Pfuhlmann, Jan Peter: PfuhJP
+Philippi, Sebastian: PhilS
+Pichl, Carolin: PichC
+Pitz, Volker: PitzV
+Pohl, Sebastian: PohlS
+Pointeau, Dieter: PoinD
+Pollmächer, Birte: PollB
+Poloczek, Alexandra Anna: PoloAA
+Poloczek, Fabian Michael: PoloFM
+Pöpperl, Carina Michelle: PöppCM
+Posmik, Günter: PosmG
+Pührer, Florian: PührF
+Pusch, Günter: PuscG
+#endregion
+
+#region R
+Raes: RaesK
+Raes, Karlheinz: RaesK
+# Ranft-Metz, Martina: RanfMM
+Raptis, Georgios: RaptG
+# Rath, Heike: Rath
+Rausch, Jan Steffen: RausJ
+Rehberg, Martin: RehbM
+# Reichel, Thorsten:
+Reichel-Wald, Katharina: ReicK
+# Reineke, Kristin: ReinK
+# Reining: Reining
+# Reinold: Reinold (ReinT is used.)
+Reimers, Hayo: ReimH0
+Reimers: ReimK
+Reimers: ReimM
+Reimers, Marie-Madeleine: ReimMM
+Rein, Andre: ReinA
+Reindl, Christian: ReinC
+Reineke, Kristin: ReinK
+Reiter, Anne: Reiter
+Reiter, Anne Marlene: ReitAM
+# Reyes, Angelica: Reyes
+Richarts: RichF #manual removal
+Richarts, Frtiz: RichF #manual removal
+Richarts, Fritz: RichFt
+# Richter: Richter
+# Rickards, R:
+# Rieber, Susanne: RiebS
+Ried, Malte: RiedM
+# Rienhardt, Wolf: RienW
+Riekmeier, Jens: RiekJ
+# Robinson: Robinson
+# Röder, Jasmin: RödeJ
+# Rost, Harald: RostH
+# Roth, Manuel: RothM
+# Roth, Richard:
+# Rubinich: Rubinich
+# Rüger, Christian: Rüger
+# Rühl, Christian: RuehC
+Rupp, Oliver: RuppO
+Rüweler, Dörte: RüweD
+
+
+Rost, HaraldRostH0
+Roth, Karl-JosephRothKJ0
+RothRothKJ0
+Roth, ManuelRothM0
+Ruhl, FabianRuhlF0
+Rumpf, MariaRumpM0
+Rupp, SteffenRuppS0
+Röder, JasminRödeJ0
+Rühl, Marie-LuiseRühlML0
+#endregion
+
+#region S
+# Sachs: Sachs
+# Sauerer, David: SaueDa
+# Schäfer: Schäfer
+# Schärer-Pohlmann, Günter: Schärer
+# Scheer, Wolfgang: ScheW
+# Scheppat, Rick: ScheR
+# Scherer: ScheS
+# Schiffke: Schiffke
+# Schlapp, Ernst-Dieter: SchlED
+# Schlösser-Hold, Dorinda: SchlHD
+# Schlotmann, Jonas: SchlJns
+# Schmidts, Anja: Schmidts
+# Schmiedel: Schmiedel
+# Schmitz, Michel Jonathan: SchmMJ
+# Schneider, Patrick: SchnP
+# Schoeffling, Helmut: SchoHe
+# Schröder, Theresa: SchrT
+# Schübbe, Sabrina: SchüS
+# Schuler, Konstantin:
+# Sczecina: Sczecina
+# Seiferth: Seiferth
+# Seiler, Josef: SeilJ
+# Seitz, Christian: SeitC
+# Selzer, Günter: SelzG
+# Sendzik, Michaela: SendM
+# Shaker, Mohammed: ShakM
+# Sieber, Urs: SiebU
+# Siefert, Eva: Siefert
+# Siegert, Maria: SiegM
+# Siegrid, Ortwein: OrtweinS
+# Simon: Simon
+# Simon, George: SimoG
+# Simonis, Niklas: SimoN
+# Simsek, Umutçan: Simsek
+# Slemeyer, Andreas: SlemA
+# Spengler, Jan-Peter: SpenJP
+# Stadlbauer, Ernst:
+# Stamm, Markus: StamM
+# Stamminger: Stamminger
+# Stauss-Grabo, Manuela: StauGM
+# Steinhardt: Steinhardt
+# Stoll, Sebastian: StolS
+# Störzel, Klaus: StörK
+# Stroh: Stroh
+# Strunck: Strunck
+# SuK Dozent: NN.SUK
+# Sutter, Hermann: SuttH
+# Swierczyna, Stephen: Swierczyna
+#endregion
+
+# region T
+# T.: Niederhaus
+# Täntzer, Gabriele: TäntG
+# Tauber, Andreas: TaubA
+# Telli, Can: TellC
+# Teymoortash, Neda: Teymoortash
+# THM, thm: thm
+# Tietje, Johannes: TietJ
+# Titzmann, Thorsten: TitzT
+# Trampisch, Werner: TramW
+# Trierweiler, Hans-Georg: TrieHG
+# Tyne, Grace Tjani: TyneGT
+#endregion
+
+#region U
+# Uhrig: Uhrig
+# Ullmann, Klaus: Ullmann
+# Ullrich, Matthias: UllrM
+# Unbekannt: UNB
+# Urich: Urich
+# Urich, Kira Vanessa: UricKV
+# Usoltseva, Vera: Usoltseva
+#endregion
+
+#region V
+# Valentin: Val
+# Valentin, Anke: ValeA
+# Vetter, Jochen: Vett
+# Vieth, Philipp Maximilian: Vieth
+# Vogel, Hans-Jürgen: VogelH
+# Voigt, Johannes: VoigJ
+# von Helden, Marya: vonHelden
+# von Marcard, Gisa:
+# von Pokorny, Matthew: von Pokorny
+# von Steldern: von Steldern
+# von Steldern, Dierk: vStelD
+# von Straelen, Wolfgang: vStraelen
+# Freifrau von Weichs, Ines-Maria: vWeichs
+#endregion
+
+#region W
+# Wagner, Antonia: WagnA
+# Wagner, Georg: WagnG
+# Wagner, Janna Dorothea: Wagner
+# Wagner, Julia: WagnJ
+# Waldbröhl: Waldbröhl
+# Walesch, Christoph: WaleC
+# Walter, Hans W.: WaltHW
+# Warmbold, Alexander: WarmA
+# Wawrzinek: Wawrzinek
+# Weber, Bernd: WebeBer
+# Weidner, Tobias: Weidner
+# Weigand, Erik: WeigE
+# Weissflog: Weiss
+# Weller: Weller
+# Wember, Theo: WembT
+# Werner: Wern
+# Werner, Trampisch: WernT
+# Weselek: Weselek
+# Weselek, Linda: WeseL
+# White, Guy: WhitG
+# Wichmann, Jörg: WichJ
+# Willhelm, Jörg: WillJ
+# Wirtz, Anna:
+# Witte, Yanet: WittY
+# Wolf, Hans: WolfH
+# Wörner, Sascha: WörnS
+# Wurster, Carmen: WursC
+#endregion
+
+#region Z
+# Ziehdorn, Doreen: ZiehD
+# Zimmermann: Zimm
+# Zinecker, TinaZ ineT
+# Zinke, Joachim ZinkJ
+# Zschiegner, Marc-Alexander: ZschMA
+#endregion
+
+# region Untis Crashes on Delete
+--------------------------------------------------------------
+Krause, Günther: KrauseG
+Loth, Michael: LothMi
+Müller, König: MüllK
+Niksch, Christine: Niksch
+Ochs-Held, Klaus-Peter: OchsHKP
+Schröder, Catthrin: SchrC
+THM Dozent: NN.DOZ
+#endregion
+
+
+
+Sadowski, HeinrichSadoH0
+SadowskiSadoH0
+Salzig, DeniseSalzD0
+Sauer, DominikSaueD0
+Sauerer, DavidSaueDa0
+Scheppat, RickScheR0
+Scheer, WolfgangScheW0
+ScheerScheW0
+Schlapp, Ernst-DieterSchlED0
+Schlösser-Hold, DorindaSchlHD0
+Schlotmann, JonasSchlJns0
+Schmidt, BjörnSchmB0
+Schmidts, ThomasSchmT0
+Schneider, PatrickSchnP0
+SchneiderSchnP0
+Schomber, HartmutSchoHa0
+SchoefflingSchoHe0
+Schröder, CathrinSchrCa0
+Schröder, TheresaSchrT0
+Schummer, DietmarSchuD0
+Schummer, DietmarSchuDt0
+Schulz, Jens-UweSchuJU0
+Schurr, MartinSchuM0
+Schäfer, BenjaminSchäB0
+Schäfer, ReginaSchäR0
+Schönhals, HorstSchöH0
+Schübbe, SabrinaSchüS0
+SczecinaScze0
+Seiler, JosefSeilJ0
+SeilerSeilJ0
+Seitz, ChristianSeitC0
+Selzer, GünterSelzG0
+Sendzik, MichaelaSendM0
+Shaker, MohammedShakM0
+Sidki, MarcusSidkM0
+Siegert, MariaSiegM0
+Simon, GeorgeSimoG0
+Simonis, NiklasSimoN0
+Slemeyer, AndreasSlemA0
+Sohrabi, AliSohrA0
+Spengler, Jan-PeterSpenJP0
+Stadlbauer, Ernst A.StadEA0
+StammStamM0
+Stamm, MarkusStamM0
+Stauss-Grabo, ManuelaStauGM0
+SteinhäuserStei0
+Stoll, SebastianStolS0
+Störzel, KlausStörK0
+StörzelStörK0
+Sutter, HermannSuttH0
+TauberTaub0
+Tauber, AndreasTaubA0
+Telli, CanTellC0
+TheilenThei0
+Theuerkauf, Judith S.TheuJS0
+ThiesThie0
+Thies, Karl-HeinzThieKH0
+Tietje, JohannesTietJ0
+Titzmann, ThorstenTitzT0
+Trampisch, WernerTramW0
+TrierweilerTrieHG0
+Trierweiler, Hans-GeorgTrieHG0
+Tyne, Grace TjaniTyneGT0
+Tyne, TjaniTyneT0
+Täntzer, GabrieleTäntG0
+Ullrich, MatthiasUllrM0
+Ullrich, TobiasUllrT0
+Unold, FlorianUnolF0
+Urich, Kira VanessaUricKV0
+Valentin, AnkeValeA0
+Vogel, Hans-JürgenVogeHJ0
+Voigt, JohannesVoigJ0
+VolkmerVolk0
+Voss, PeterVossP0
+Wagner, AntoniaWagnA0
+Wagner, GeorgWagnG0
+WagnerWagnJ0
+Wagner, JuliaWagnJ0
+WaldingerWald0
+Waldvögel-Röcker, KerstinWaldRK0
+Walesch, ChristophWaleC0
+Wallisch, StefanWallS0
+Walter, Hans W.WaltHW0
+WalterWaltHW0
+Warmbold, AlexanderWarmA0
+Wassermann, UlrikeWassU0
+Wawrzinek, LenaWawrL0
+Weber, BenediktWebeB0
+Weber, BenediktWebeBen0
+Weber, BerndWebeBer0
+Weber, InaWebeI0
+Weigand, ErikWeigE0
+Weigand, HaraldWeigHa0
+WeiselWeis0
+Welker, AntjeWelkA0
+Wember, TheoWembT0
+WernerWern0
+Werner, TrampischWernT0
+Werner, WilhelmWernW0
+Weselek, LindaWeseL0
+White, GuyWhitG0
+Wichmann, JörgWichJ0
+Willhelm, JörgWillJ0
+WillhelmWillJ0
+Wirtz, AnnaWirtA0
+Witte, YanetWittJ0
+Witte, YanetWittY0
+Wolf, HansWolfH0
+Wörner, SashaWörnS1
+
+von MarcardvMarG0
+von Pokorny, Matthew LazlovPokML0
+von Steldern, DierkvStelD0
+von Weichs, InesvWeiI0
+Freifrau von Weichs, Ines-MariavWeiIM0
