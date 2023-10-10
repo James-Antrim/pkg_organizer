0ce09e4f76d67b3a2953cf24c8e3745c91e6a51e
James Antrim <james.antrim@nm.thm.de>
2023-10-10 11:55:15 +0200

    Added shell for later implementation of an HTML adapter.

---
+++ com_organizer/site/Adapters/HTML.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * @package     Groups
+ * @extension   com_groups
+ * @author      James Antrim, <james.antrim@nm.thm.de>
+ * @copyright   2022 TH Mittelhessen
+ * @license     GNU GPL v.3
+ * @link        www.thm.de
+ */
+
+namespace THM\Groups\Adapters;
+
+use Joomla\CMS\HTML\HTMLHelper;
+use Joomla\CMS\Language\Text;
+use Joomla\Utilities\ArrayHelper;
+
+class HTML extends HTMLHelper
+{
+
+}
