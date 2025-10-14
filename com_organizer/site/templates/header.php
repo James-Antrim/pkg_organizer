3c8344c2a69f76002cb1040c8827a17ab5ea20ab
James Antrim <james.antrim@nm.thm.de>
2025-10-14 09:03:52 +0200

    Added changes from live-organizer. -Untested

--- com_organizer/site/templates/header.php
+++ com_organizer/site/templates/header.php
@@ -8,11 +8,44 @@
  * @link        www.thm.de
  */
 
-use THM\Organizer\Adapters\{Application, Toolbar};
+use Joomla\CMS\Uri\Uri;
+use THM\Organizer\Adapters\{Application, HTML, Text, Toolbar};
 use THM\Organizer\Views\HTML\Titled;
 
 /** @var Titled $this */
 if (!Application::backend()) {
+    if (Application::dynamic()) {
+        $URI = Uri::getInstance();
+
+        // Remove from query
+        $query = $URI->getQuery(true);
+        unset($query['lang']);
+
+        // Remove from path w/o query
+        $URL = $URI->toString(['scheme', 'user', 'pass', 'host', 'port', 'path', 'fragment']);
+        $URL = preg_replace('/\/[a-z]{2}\/?$/', '/', $URL);
+
+        if (Application::tag() === 'en') {
+            $label = Text::_('GERMAN');
+            $tag   = 'de';
+        }
+        else {
+            $label = Text::_('ENGLISH');
+            $tag   = 'en';
+        }
+
+        if (Application::configuration()->get('sef', 0)) {
+            $URL = preg_replace('/\/(index\.php|)$/', "/$tag", $URL);
+        }
+        else {
+            $query['lang'] = $tag;
+        }
+
+        $URL .= '?' . http_build_query($query);
+
+        $icon = HTML::icon('fa fa-earth');
+        echo '<div class="right">' . HTML::link($URL, $icon . $label) . '</div>';
+    }
     echo "<h1>$this->title</h1>";
     echo $this->subtitle ? "<h4>$this->subtitle</h4>" : '';
     echo $this->supplement;
