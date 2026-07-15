288b0701a8d22fe243307d0df624d360a282c297
James Antrim <james.antrim@nm.thm.de>
2026-07-15 10:00:08 +0200

    Removed debug output.

--- com_organizer/site/Helpers/Coded.php
+++ com_organizer/site/Helpers/Coded.php
@@ -19,7 +19,7 @@ trait Coded
      * Attempts to resolve a given code to an id or id to code.
      *
      * @param int|string $value the id of the resource
-     * @param string $name the name corresponding to the value in the program mapping
+     * @param string     $name  the name corresponding to the value in the program mapping
      * @return int|string|null
      */
     public static function code(int|string $value, string $name): int|null|string
@@ -35,9 +35,9 @@ trait Coded
         if (is_string($value)) {
             if ($table->load(['code' => $value])) {
                 return $table->id;
-            } else {
+            }
+            else {
                 $identifier = Application::uqClass(self::class);
-                echo "<pre>Type $identifier Value $value does not exist.</pre>";
                 Application::message(Text::sprintf('IDENTIFIER_UNKNOWN', $identifier, $value, $name));
             }
         }
